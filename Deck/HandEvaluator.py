4253bc9ee82448405b0aef0891a035446dcaa711
dj.santiago25 <dj.santiago25@upr.edu>
2025-12-08 20:58:08 -0400

    Christopher TASK 3

--- Deck/HandEvaluator.py
+++ Deck/HandEvaluator.py
@@ -10,4 +10,86 @@ from Cards.Card import Card, Rank
 #   and flags to determine if the hand is: "Four of a Kind", "Full House", "Flush", "Straight", "Three of a Kind",
 #   "Two Pair", "One Pair", or "High Card". Return a string with the correct hand type at the end.
 def evaluate_hand(hand: list[Card]):
+    ranks = []
+    suits = []
+    for card in hand:
+        ranks.append(card.rank.value)
+        suits.append(card.suit)
+    rank_counts = {}
+    for rank in ranks:
+        if rank in rank_counts:
+            rank_counts[rank] += 1
+        else:
+            rank_counts[rank] = 1
+
+    sorted_counts = sorted(rank_counts.values(), reverse = True)
+
+    suit_counts = {}
+    for suit in suits:
+        if suit in suit_counts:
+            suit_counts[suit] += 1
+        else:
+            suit_counts[suit] = 1
+
+
+    flush = False
+    flush_suit = None
+    for suit, count in suit_counts.items():
+        if count >= 5:
+            flush = True
+            flush_suit = suit
+            break
+
+    unique_ranks = sorted(list(set(ranks)))
+
+    straight = False
+    straight_rank = []
+
+    for i in range(len(unique_ranks)-4):
+        if (unique_ranks[i+1] == unique_ranks[i] + 1 and
+            unique_ranks[i+2] == unique_ranks[i] + 2 and
+            unique_ranks[i+3] == unique_ranks[i] + 3 and
+            unique_ranks[i+4] == unique_ranks[i] + 4):
+            is_straight = True
+            straight_ranks = unique_ranks[i:i+5]
+            break
+
+
+    if not straight and 14 in unique_ranks:
+        if 2 in unique_ranks and 3 in unique_ranks and 4 in unique_ranks and 5 in unique_ranks:
+            straight = True
+            straight_ranks = [1, 2, 3, 4, 5]
+
+
+    if straight and flush:
+        straight_in_flush_suit = True
+        for card in hand:
+            if card.rank.value in straight_rank and card.suit != flush_suit:
+                straight_in_flush_suit = False
+                break
+
+        if straight_in_flush_suit:
+            return "Straight Flush"
+
+    if sorted_counts[0] == 4:
+        return "Four of a Kind"
+
+    if sorted_counts[0] == 3 and sorted_counts[1] == 2:
+        return "Full House"
+
+    if flush:
+        return "Flush"
+
+    if straight:
+        return "Straight"
+
+    if sorted_counts[0] == 3:
+        return "Three of a Kind"
+
+    if sorted_counts[0] == 2 and sorted_counts[1] == 2:
+        return "Two Pair"
+
+    if sorted_counts[0] == 2:
+        return "One Pair"
+
     return "High Card" # If none of the above, it's High Card
